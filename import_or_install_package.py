f2c7a88ba801d1fa3c516c054e6cdd50d5d1aedc
thuzax <arthurhscruz@gmail.com>
2024-05-28 14:59:01 -0300

    instala numpy

--- import_or_install_package.py
+++ import_or_install_package.py
@@ -1,17 +1,26 @@
-import pip
-import importlib.util
 
 def install_package(package):
-    text = "O pacote numpy é necessário para a execução. " 
+    import os
+    text = "O pacote " + str(package) + " é necessário para a execução. " 
     text += "Deseja instalá-lo [S/n]? "
     answer = input(text)
-    print(answer == "S")
     if (answer != "s" and answer != "S" and answer != ""):
-        print("Numpy é necessário para a execução. Finalizando.")
+        text = "O pacote " 
+        text += str(package) 
+        text += " é necessário para a execução. Finalizando."
+        print(text)
         exit(0)
-    pip.main(['install', package])
+    print("Tentando instalar o pacote " + str(package))
+    result = os.system("sudo apt install python-" + str(package))
+    result = os.system("sudo apt install python3-" + str(package))
+    if (result != 0):
+        print("Não foi possível instalar o pacote " + str(package))
+        print("Será necessário fazer a instalação manual.")
+        print("Reexecute o programa após finalizar a instalação")
+        exit(0)
 
 def verify_package_installed(package):
+    import importlib.util
     spec_result = importlib.util.find_spec(package)
     if (spec_result is None):
         return False
